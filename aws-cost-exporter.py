f8eda1ce451d2df3f2adb5b7d19d5e9de5c8f035
Gabriel Noguchi <gabriel1tdsc@gmail.com>
2020-08-30 19:48:23 -0300

    ref code to attend our need

--- aws-cost-exporter.py
+++ aws-cost-exporter.py
@@ -6,6 +6,7 @@ import os
 import re
 import boto3
 import datetime
+import json, ast
 
 # Try get the TAG_PROJECT env variable. If not defined, we will use the Scost
 tagProject = os.getenv('TAG_PROJECT', 'Name')
@@ -40,14 +41,18 @@ def getCosts():
     response = client.get_cost_and_usage(
         TimePeriod={
             'Start': start,
-            'End':  end
+            'End': end
         },
         Granularity='HOURLY',
         Metrics=['BlendedCost'],
         GroupBy=[
             {
                 'Type': 'TAG',
-                'Key': tagProject
+                'Key': 'Product'
+            },
+            {
+                'Type': 'TAG',
+                'Key': 'App'
             }
         ]
     )
@@ -57,21 +62,43 @@ def getCosts():
 
     # Run the response and make a dictionary with tag name and tag value
     for project in response["ResultsByTime"]:
-        
-        # Get the value
-        group = project["Groups"]
 
-        if not group:
+        groups = project["Groups"]
+
+        if not groups or groups is None:
             continue
-        amount = group[0]['Metrics']['BlendedCost']['Amount']
-        
-        # Format the time to 0.2 points
-        amount = "{0:.2f}".format(float(amount))
 
-        # Append the values in the directionary
-        projectValues.append(float(amount))
+        for group in groups:
+            objectValues = {}
 
-    # Return the dictionary with all those values
+            keys = group['Keys']
+            product = keys[0]
+            app = keys[1]
+            product_value = product.split("Product$", 1)[1]
+            app_value = app.split("App$", 1)[1]
+
+            unincoded_product_value = ast.literal_eval(json.dumps(product_value))
+            unincoded_app_value = ast.literal_eval(json.dumps(app_value))
+          
+
+            if (not unincoded_product_value or unincoded_product_value is None or unincoded_product_value == '') and (not unincoded_app_value or unincoded_app_value is None or unincoded_app_value == ''):
+                print('ignoring current resource')
+                continue
+
+            amount = group['Metrics']['BlendedCost']['Amount']
+            unincoded_cost_value = ast.literal_eval(json.dumps(amount))
+
+            objectValues = { 
+                "app": unincoded_app_value,
+                "product": unincoded_product_value,
+                "value": unincoded_cost_value
+            }
+
+            # Append the values in the directionary
+            projectValues.append(objectValues)
+
+        return projectValues
+
     return projectValues
 
 # Start classe collector
@@ -86,8 +113,8 @@ class costExporter(object):
 
         # Run the retuned dictionary and expose the metrics
         for cost in getCosts():
-            metric.add_sample('aws_project_cost', value=cost,
-                              labels={'project': 'aws'})
+            metric.add_sample('aws_project_cost', value=cost['value'],
+                              labels={'app': cost['app'], 'product': cost['product']})
 
         # /Expose the metric
         yield metric
